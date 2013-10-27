1ea1db8e6780ddf86611123314dd3ebb1b018f75
Kenny Chapman <lkchapman@gmail.com>
2013-10-27 17:47:25 -0400

    added more menus

--- ShakeShackAndroid/src/main/java/com/chapslife/shakeshackandroid/utils/Constants.java
+++ ShakeShackAndroid/src/main/java/com/chapslife/shakeshackandroid/utils/Constants.java
@@ -7,7 +7,43 @@ public class Constants {
 
     public static final String CAMERA_URL = "http://www.shakeshack.com/camera.jpg";
     public static final String CUSTARD_URL = "http://7677mobile.com/shakeshack/custard_calendar.png";
+
+    /** CT MENUS */
+    public static final String MENU_WESTPORT = "http://www.shakeshack.com/wp-content/uploads/2013/07/SHA_Menu_8.5x11_WSPT_June-2013.jpg";
+    public static final String MENU_NEW_HAVEN = "http://www.shakeshack.com/wp-content/uploads/2013/07/SHA_Menu_8.5x11_HVN_June-2013.jpg";
+
+    /** FL MENUS */
+    public static final String MENU_S_BEACH = "http://www.shakeshack.com/wp-content/uploads/2013/07/SHA_Menu_8.5x11_MIAMI_June-2013.jpg";
+    public static final String MENU_CORAL_GABLES = "http://www.shakeshack.com/wp-content/uploads/2013/07/SHA_Menu_8.5x11_CG_JUNE-2013.jpg";
+    public static final String MENU_BOCA = "http://www.shakeshack.com/wp-content/uploads/2013/07/SHA_Menu_8.5x11_BOCA_June_2013.jpg";
+
+    /** NYC MENUS */
     public static final String MENU_MSP = "http://www.shakeshack.com/wp-content/uploads/2013/08/SHA_Menu_8.5x11_MSP_AUG-2013.jpg";
+    public static final String MENU_BPC = "http://www.shakeshack.com/wp-content/uploads/2013/07/SHA_BPC_Menu_8.5x11__June2013.jpg";
+    public static final String MENU_TD = "http://www.shakeshack.com/wp-content/uploads/2013/09/SHA_Menu_8.5x11_TD_Sept-2013.jpg";
+    public static final String MENU_UES = "http://www.shakeshack.com/wp-content/uploads/2013/09/SHA_Menu_8.5x11_UES_Sept-2013.jpg";
+    public static final String MENU_UWS = "http://www.shakeshack.com/wp-content/uploads/2013/07/SHA_Menu_8.5x11_UWS_June-2013.jpg";
+    public static final String MENU_BKLYN = "http://www.shakeshack.com/wp-content/uploads/2013/07/SS_BK_Menu_8.5x11_June-2013.jpg";
+    public static final String MENU_GST = "http://www.shakeshack.com/wp-content/uploads/2013/10/menu_gct_main_v2.jpg";
+    public static final String MENU_JFK_T4 = "http://www.shakeshack.com/wp-content/uploads/2013/10/SHA_Menu_8.5x11_JFK_Oct-2013_R1.jpg";
+    public static final String MENU_WBY = "http://www.shakeshack.com/wp-content/uploads/2013/07/SHA_Menu_8.5x11_WBY_June2013.jpg";
+    public static final String MENU_SSNY = "http://www.shakeshack.com/wp-content/uploads/2013/07/SHA_Menu_8.5x11_Saratoga_July-2013.jpg";
+    public static final String MENU_SHITI_FIELD = "http://www.shakeshack.com/wp-content/uploads/2012/04/ss_citi-field-2013_main.jpg";
+
+
+    /** PA MENUS */
+    public static final String MENU_CENTER_CITY = "http://www.shakeshack.com/wp-content/uploads/2013/08/SHA_Menu_8.5x11_PHL_AUG-2013.jpg";
+    public static final String MENU_U_CITY = "http://www.shakeshack.com/wp-content/uploads/2013/10/menu_ucity_main.jpg";
+    public static final String MENU_KOP = "http://www.shakeshack.com/wp-content/uploads/2013/10/menu_kop_main.jpg";
+
+    /** DC MENUS */
+    public static final String MENU_DUPONT_CIRCLE = "http://www.shakeshack.com/wp-content/uploads/2013/09/SHA_Menu_8.5x11_DC_Sept-2013_R1.jpg";
+    public static final String MENU_NATS_PARK = "http://www.shakeshack.com/wp-content/uploads/2013/03/ss_nats-2013_main.jpg";
+    public static final String MENU_F_STREET = "http://www.shakeshack.com/wp-content/uploads/2013/06/menu-f-street_main.jpg";
+
+    /** MASS MENUS */
+    public static final String MENU_CHESNUT_HILL = "http://www.shakeshack.com/wp-content/uploads/2013/10/menu_ch_main-v2.jpg";
+
     public static final long IMAGE_REFRESH_TIME = 5000;
 
 }
